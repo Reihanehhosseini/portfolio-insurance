73e848ac6d336f83b10e6697f75128c0f98ad00f
reihaneh hosseini <reihaneh_h1368@yahoo.com>
2026-09-18 13:26:21 +0330

    openGraph create

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,24 @@ import localFont from "next/font/local";
 
 export const metadata = {
   metadataBase: new URL("https://mosadegh-insurance.ir"),
-  alternates:{
-    canonical:"/",
+  alternates: {
+    canonical: "/",
   },
   title: "شیما مصدق | مشاور بیمه آسیا در اصفهان ",
-  description: "شیما مصدق ، مشاور بیمه آسیا با 14 سال تجربه در ارائه مشاوره و خدمات بیمه ای : بیمه اتومبیل ، مسئولیت ، آتش سوزی و سایر خدمات بیمه ای  در اصفهان",
+  description:
+    "شیما مصدق ، مشاور بیمه آسیا با 14 سال تجربه در ارائه مشاوره و خدمات بیمه ای : بیمه اتومبیل ، مسئولیت ، آتش سوزی و سایر خدمات بیمه ای  در اصفهان",
+  url: "https://mosadegh-insurance.ir",
+  siteName:"شیما مصدق",
+  locale:"fa_IR",
+  type:"website",
+  images:[
+    {
+      url:"/hero-full.PNG",
+      width:1721,
+      height:914,
+      alt:"شیما مصدق مشاور بیمه آسیا در اصفهان"
+    }
+  ]
 };
 const myFont = localFont({
   src: [
